f0ab44acbf84868921333ced217d126275e7a361
GarrettMcNaMaLang <garrett.m.lang@outlook.com>
2026-06-30 18:30:19 -0400

    Day1
    WASD 3D Movement figured out, focus tomorrow on Aiming the camera

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,4 +1,6 @@
+using UnityEditor.Callbacks;
 using UnityEngine;
+using UnityEngine.InputSystem;
 [RequireComponent(typeof(Camera))]
 public class PlayerScript : MonoBehaviour
 {
@@ -10,11 +12,20 @@ public class PlayerScript : MonoBehaviour
 
     public GameObject ballSpawner;
 
+    public Rigidbody rb;
+
+    Vector2 movementVec;
+
+    public float playerSpeed;
+
     public void Awake()
     {
         playerCamera = GetComponent<Camera>();
 
+        rb = GetComponent<Rigidbody>();
+
         ballHoldPosition.SetActive(false);
+
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -29,5 +40,24 @@ public class PlayerScript : MonoBehaviour
         
     }
 
-    
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector3(movementVec.x * playerSpeed, 0f, movementVec.y * playerSpeed);
+
+        
+    }
+
+   public void Move(InputAction.CallbackContext context)
+    {
+        Debug.Log("Moving");
+        movementVec = context.action.ReadValue<Vector2>();
+    }
+
+    public void Look(InputAction.CallbackContext context)
+    {
+        Debug.Log("Looking");
+        context.action.ReadValue<Vector2>();
+    }
+
+
 }
