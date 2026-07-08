8f1dd282802dfb85f05cda4a9bdd5b4244cc6fda
GarrettMcNaMaLang <garrett.m.lang@outlook.com>
2026-07-08 12:18:56 -0400

    Day2
    Updated player with first person looking mechanic, paddle warps with camera movement, fixed paddle collision issues, can now focus on camera and movement lag. focus on fixing lag next time, and then work on jumping, sprinting, and turning around. Maybe we should add a mini map for the balls (too much or no?)

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,12 +1,15 @@
 using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.InputSystem;
-[RequireComponent(typeof(Camera))]
+using Cinemachine;
+using UnityEngine.AI;
+
+//[RequireComponent(typeof(Camera))]
 public class PlayerScript : MonoBehaviour
 {
 
     public GameObject ballPrefab;
-    Camera playerCamera;
+    CinemachineVirtualCamera playerCamera;
     
     public GameObject ballHoldPosition;
 
@@ -14,13 +17,24 @@ public class PlayerScript : MonoBehaviour
 
     public Rigidbody rb;
 
-    Vector2 movementVec;
+    Vector2 movementVec, lookingVec;
 
-    public float playerSpeed;
+    public float playerSpeed, lookingSensitivity = 0.1f, pitchLimit = 85f;
+
+    private float currPitch = 0f;
+
+    public float CurrPitch
+    {
+        get {return currPitch;}
+
+        set {currPitch = Mathf.Clamp(value,-pitchLimit, pitchLimit);}
+    }
+
+    
 
     public void Awake()
     {
-        playerCamera = GetComponent<Camera>();
+        playerCamera = GetComponentInChildren<CinemachineVirtualCamera>();
 
         rb = GetComponent<Rigidbody>();
 
@@ -43,20 +57,26 @@ public class PlayerScript : MonoBehaviour
     void FixedUpdate()
     {
         rb.linearVelocity = new Vector3(movementVec.x * playerSpeed, 0f, movementVec.y * playerSpeed);
+        Vector2 lookInput = new Vector2(lookingVec.x * lookingSensitivity, lookingVec.y * lookingSensitivity);
 
-        
+        CurrPitch -= lookInput.y;
+
+        playerCamera.transform.localRotation = Quaternion.Euler(CurrPitch,0f, 0f);
+
+        rb.transform.Rotate(Vector3.up * lookInput.x);
     }
 
    public void Move(InputAction.CallbackContext context)
     {
-        Debug.Log("Moving");
+        //Debug.Log("Moving");
         movementVec = context.action.ReadValue<Vector2>();
     }
+
 
     public void Look(InputAction.CallbackContext context)
     {
         Debug.Log("Looking");
-        context.action.ReadValue<Vector2>();
+       lookingVec = context.action.ReadValue<Vector2>();
     }
 
 
